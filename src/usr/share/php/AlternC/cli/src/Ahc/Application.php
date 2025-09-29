0a911d30b1f7e14fdcfd4fd548c299509d63a2ea
cam.lafit <cam.lafit@azerttyu.net>
2025-09-29 15:39:08 +0200

    Support multi argument command
    * As workaround to manage multi argument context
    * Related to upstream issue : https://github.com/adhocore/php-cli/issues/132

---
+++ src/usr/share/php/AlternC/cli/src/Ahc/Application.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AlternC\Ahc;
+
+use Ahc;
+use Ahc\Cli\Input\Command;
+
+class Application extends Ahc\Cli\Application
+{
+    public function commandFor(array $argv): Command
+    {
+        $argv += [null, null, null];
+        $command = null;
+
+        //sort by key length
+        //find maximal command compatibility by arguments
+        $keys = array_map('strlen', array_keys($this->commands));
+        array_multisort($keys, SORT_ASC, $this->commands);
+
+        foreach ($this->commands as $command_name => $value) {
+            $nb_args = count(explode(' ', $command_name));
+            $args = implode(' ', array_slice($argv, 1, $nb_args));
+            if (! empty($this->commands[$args])) {
+                $command = $this->commands[$args];
+            }
+        }
+        return
+            //cmd found by multi arguments
+            $command
+            // cmd
+            ?? $this->commands[$argv[1]]
+            // cmd alias
+            ?? $this->commands[$this->aliases[$argv[1]] ?? null]
+            // default.
+            ?? $this->commands[$this->default];
+    }
+}
